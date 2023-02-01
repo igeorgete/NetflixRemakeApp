6805b295a0e74141059f1b946c2ee91acfb3d7d7
Igor <igor.georgete@kroton.com.br>
2023-02-01 18:38:55 -0300

    first commit

---
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -0,0 +1,56 @@
+package co.tiagoaguiar.netflixremake
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+import co.tiagoaguiar.netflixremake.util.CategoryTask
+
+class MainActivity : AppCompatActivity(), CategoryTask.Callback {
+
+    private lateinit var progress: ProgressBar
+    private lateinit var adapter: CategoryAdapter
+    private val categories = mutableListOf<Category>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        progress = findViewById(R.id.progress_main)
+
+        adapter = CategoryAdapter(categories) { id ->
+            val intent = Intent(this@MainActivity, MovieActivity::class.java)
+            intent.putExtra("id", id)
+            startActivity(intent)
+        }
+        val rv: RecyclerView = findViewById(R.id.rv_main)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
+
+        CategoryTask(this).execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=858d32f1-a88a-4df1-a129-ff2ace5dd50f")
+    }
+
+    override fun onPreExecute() {
+        progress.visibility = View.VISIBLE
+    }
+
+    override fun onResult(categories: List<Category>) {
+        // aqyi será quando o CategoryTask chamará de volta (callback) - listener
+        this.categories.clear()
+        this.categories.addAll(categories)
+        adapter.notifyDataSetChanged() // força o adapter chamar de novo o onBindViewHolder
+        progress.visibility = View.GONE
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        progress.visibility = View.GONE
+    }
+}
